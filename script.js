b97d6f3b2855525faea58ab05c6c2c814aaba1dd
manuelrosalesg <191149748+manuelrosalesg@users.noreply.github.com>
2025-01-23 21:49:47 -0600

    casi terminado

---
+++ script.js
@@ -0,0 +1,34 @@
+// Selecciona el body y el #content
+const body = document.querySelector('body');
+const content = document.querySelector('#content');
+const box = document.querySelector('#black-box');
+
+
+// Cambia el color del body cuando el cursor entra en el body
+body.addEventListener('mouseenter', () => {
+    body.style.backgroundColor = '#afafaf'; // Cambia a gris
+    box.style.backgroundColor = '#afafaf';
+    body.style.transform = 'scale(1.05)';
+});
+
+// Restaura el color del body cuando el cursor sale del body
+body.addEventListener('mouseleave', () => {
+    body.style.backgroundColor = '#e1e1e1'; // Vuelve a blanco
+    box.style.backgroundColor = '#000000';
+    body.style.transform = 'scale(1)';
+});
+
+// Evita que el body cambie de color cuando el cursor está sobre el #content
+content.addEventListener('mouseenter', () => {
+    body.style.backgroundColor = '#e1e1e1'; // Mantiene el color blanco
+    box.style.backgroundColor = '#000000';
+    body.style.transform = 'scale(1)';
+});
+
+content.addEventListener('mouseleave', () => {
+    body.style.backgroundColor = '#afafaf'; // Cambia a blanco cuando el cursor sale del #content
+    box.style.backgroundColor = '#afafaf';
+    body.style.transform = 'scale(1.05)';
+});
+
+
